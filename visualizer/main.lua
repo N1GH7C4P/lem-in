78c225e1ac5ca515073cef97c7693e0b52e66dba
N1GH7C4P <kimmo.polojarvi@gmail.com>
2022-12-14 20:16:46 +0200

    visualizer draws rooms correctly

--- visualizer/main.lua
+++ visualizer/main.lua
@@ -1,4 +1,3 @@
-
 local function split (inputstr, sep)
         if sep == nil then
                 sep = "%s"
@@ -12,10 +11,12 @@ local function split (inputstr, sep)
         return t
 end
 local function get_room_by_name(name)
-	for room in ANTFARM.rooms do
-		if room.name == name then
-			return room
+	local i = 0
+	while ANTFARM.rooms[i] do
+		if ANTFARM.rooms[i].name == name then
+			return ANTFARM.rooms[i]
 		end
+		i = i + 1
 	end
 end
 
@@ -28,14 +29,37 @@ local function parse_room(line)
 	return room
 end
 
+local function parse_moves(line)
+	local words = {}
+	words = split(line, " ");
+	return words
+end
+
+local function parse_tubes(line)
+	local tube = {}
+	local words = {}
+	words = split(line, "-");
+	tube.tube_start = get_room_by_name(words[0])
+	tube.tube_end = get_room_by_name(words[1])
+	return tube
+end
+local function draw_rooms()
+	local i = 0
+	while i < ANTFARM.nb_of_rooms - 1 do
+		love.graphics.circle("fill", ANTFARM.rooms[i].x * LENGTH_MULTIPLIER, ANTFARM.rooms[i].y * LENGTH_MULTIPLIER, ROOM_RADIUS)
+		i = i + 1
+	end
+end
 function love.load()
+	ROOM_RADIUS = 10
+	LENGTH_MULTIPLIER = 30
 	ANTFARM = {}
 	ANTFARM.rooms = {}
 	ANTFARM.tubes = {}
-	ANTFARM.moves = {}
+	ANTFARM.rounds_of_moves = {}
 	ANTFARM.nb_of_rooms = 0
 	ANTFARM.nb_of_tubes = 0
-	ANTFARM.nb_of_moves = 0
+	ANTFARM.nb_rounds_of_moves = 0
 
 	local i = 0;
 	local next_start, next_end;
@@ -43,10 +67,10 @@ function love.load()
 		if i == 0 then
 			ANTFARM.nb_of_ants = line
 		elseif string.find(line, "L") then
-			ANTFARM.moves[ANTFARM.nb_of_moves] = line
-			ANTFARM.nb_of_moves = ANTFARM.nb_of_moves + 1
+			ANTFARM.rounds_of_moves[ANTFARM.nb_rounds_of_moves] = parse_moves(line)
+			ANTFARM.nb_rounds_of_moves = ANTFARM.nb_rounds_of_moves + 1
 		elseif string.find(line, "-") then
-			ANTFARM.tubes[ANTFARM.nb_of_tubes] = line
+			ANTFARM.tubes[ANTFARM.nb_of_tubes] = parse_tubes(line)
 			ANTFARM.nb_of_tubes = ANTFARM.nb_of_tubes + 1
 		elseif string.find(line, "##start") then
 			next_start = 1
@@ -65,10 +89,9 @@ function love.load()
 		end
 		i = i + 1
 	end
-	print(ANTFARM.rooms[1].x)
 end
 function love.draw()
-	
+	draw_rooms()
 end
 
 function love.update()
